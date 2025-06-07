db0e02195fe29a3093fc751706cfaecde6cde112
mzaky01 <mirzazaki499@gmail.com>
2025-06-07 10:24:26 +0700

    Memperbaiki file routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -4,7 +4,7 @@ import FeaturesPage from "../pages/features/features";
 import LoginPage from "../pages/auth/login/login-page";
 import RegisterPage from "../pages/auth/register/register-page";
 import AddPage from "../pages/add/add-page";
-import CareerPage from "../pages/history/history-page";
+import HistoryPage from "../pages/history/history-page";
 import NotFoundPage from "../pages/not-found/not-found-page";
 import { checkAuthenticatedRoute, checkUnauthenticatedRouteOnly } from "../utils/auth";
 import ProfilePage from "../pages/profile/profile-page";
@@ -16,7 +16,7 @@ const routes = {
   "/login": () => checkUnauthenticatedRouteOnly(new LoginPage()),
   "/register": () => checkUnauthenticatedRouteOnly(new RegisterPage()),
   "/add": () => checkAuthenticatedRoute(new AddPage()),
-  "/career": () => checkAuthenticatedRoute(new CareerPage()),
+  "/career": () => checkAuthenticatedRoute(new HistoryPage()),
   "/profile": () => checkAuthenticatedRoute(new ProfilePage()),
   "*": new NotFoundPage(),
 };
